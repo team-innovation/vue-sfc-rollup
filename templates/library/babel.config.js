ea2e5f9708c5cc20f85477826bd9c8b0ea5b778f
Michael Dodge <michael.dodge@vivint.com>
2020-01-23 18:08:07 -0700

    Remove babel plugins that are now in babel core

--- templates/library/babel.config.js
+++ templates/library/babel.config.js
@@ -5,8 +5,4 @@ module.exports = {
     '@babel/preset-typescript',
 <% } -%>
   ],
-  plugins: [
-    '@babel/plugin-proposal-optional-chaining',
-    '@babel/plugin-proposal-nullish-coalescing-operator',
-  ],
 };
